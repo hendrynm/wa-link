8263165f915fb502e27c61833fb4555c32aa5c59
Hendry Naufal Marbella <hendrynm@users.noreply.github.com>
2020-08-28 14:58:19 +0700

    Change card to alert
    Change card class to alert class to give more impression, and some layout fix

--- app.js
+++ app.js
@@ -9,19 +9,24 @@ function buatLink() {
     isiPesan = isiPesan.split("\n")
     isiPesan = isiPesan.join("%0A")
     link += ("&text=" + isiPesan);
-    document.getElementById("link").innerHTML = link;
+    document.getElementById("link").innerHTML = "<a class='card alert alert-success' href='" +
+        link + "' target='_blank'>" + link + "</a>";
 }
 
 function validasi() {
     let nomorWhatsApp = document.getElementById("nomor").value;
     if(nomorWhatsApp===""){
-        document.getElementById("link").innerHTML = "Nomor WhatsApp tidak boleh kosong";
+        document.getElementById("link").innerHTML = "<div class='card alert alert-danger'>" +
+            "Nomor WhatsApp tidak boleh kosong</div>";
     } else if(!(nomorWhatsApp.includes("+628"))){
-        document.getElementById("link").innerHTML = "Nomor WhatsApp yang kamu masukkan tidak diawali dengan \"+628\"";
+        document.getElementById("link").innerHTML = "<div class='card alert alert-danger'>" +
+            "Nomor WhatsApp yang kamu masukkan tidak diawali dengan +628</div>";
     } else if(nomorWhatsApp.length<12){
-        document.getElementById("link").innerHTML = "Nomor WhatsApp yang kamu masukkan kurang dari 10 digit.";
+        document.getElementById("link").innerHTML = "<div class='card alert alert-danger'>" +
+            "Nomor WhatsApp yang kamu masukkan kurang dari 10 digit.</div>";
     } else if(nomorWhatsApp.length>15){
-        document.getElementById("link").innerHTML = "Nomor WhatsApp yang kamu masukkan lebih dari 13 digit.";
+        document.getElementById("link").innerHTML = "<div class='card alert alert-danger'>" +
+            "Nomor WhatsApp yang kamu masukkan lebih dari 13 digit.</div>";
     } else{
         buatLink();
     }
